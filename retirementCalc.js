86fd92616e9d5f7401c14478409cbf1063ff48fa
BENZABOSS <joyceben33@gmail.com>
2019-12-14 17:14:15 -0500

    Add thousands separator

--- retirementCalc.js
+++ retirementCalc.js
@@ -4,7 +4,7 @@
 
 
 
-const calcFunction = function(percentOfSalary, averageReturnRate, averageSalary, years) {
+const calcFunction = function (percentOfSalary, averageReturnRate, averageSalary, years) {
 
 
     //Variables Declared
@@ -22,28 +22,37 @@ const calcFunction = function(percentOfSalary, averageReturnRate, averageSalary,
     const calcEndBalance = function (toSave, interest) {
         const interestMultiplier = (1 + interest);
 
-        for (let i = 0; i < years; i++ ) {
+        for (let i = 0; i < years; i++) {
             if (i > 0) {
                 total *= interestMultiplier;
             }
 
             total += toSave;
-            console.log(total);
+            
         };
-    
 
-        return total.toFixed(2);
-        
+        return total;
+
     };
 
-   const endBalance = calcEndBalance(salaryToSave, averageReturnRate);
+    // IIFE: set equal to a function being invoked (sets a variable that requires multiple operations)
+    const endBalance = function() {
+        let balance = calcEndBalance(salaryToSave, averageReturnRate);
+        balance = Math.round(balance);
+        balance = thousands_separators(balance);
+        return balance;
+    }();
 
+    function thousands_separators(num) {
+        var num_parts = num.toString().split(".");
+        num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        return num_parts.join(".");
+    }
 
-
-    console.log(`With an average salary of $${averageSalary} over the next ${years} years, you will have $${endBalance} if you invest ${pPOS}% of your post-tax income with an ${percentARR}% return.`)
+    console.log(`With an average salary of $${thousands_separators(averageSalary)} over the next ${years} years, you will have $${endBalance} if you invest ${pPOS}% of your post-tax income with an ${percentARR}% return.`)
 };
 
- 
+
 
 
 calcFunction(.20, .08, 50000, 20);
